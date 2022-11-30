dd9ba2e26d6779b20bcd02f22436f58a4842cf84
antonio-crispino <77855308+antonio-crispino@users.noreply.github.com>
2022-11-30 13:18:48 -0500

    Implement search_for_solution for algo class

--- implementation/generic_search_algorithm.py
+++ implementation/generic_search_algorithm.py
@@ -15,18 +15,35 @@ class GenericSearchAlgorithm(ABC):
     initial_node = Node.generate_initial_node(initial_gamestate, use_g, use_h, use_f, heuristic)
     self.open_list = [initial_node]   # The list that contains all open nodes (nodes to be visited/expanded)
     self.closed_list = []             # The list that contains all closed nodes (nodes that were visited/expanded)
+    self.number_of_steps = 1          # The number of steps that have taken place in the algorithm
 
-  # ----------------
-  # Abstract Methods
-  # ----------------
+  def print_current_state(self):
+    """
+    Method to print the current state of the algorithm (open and closed lists)
+    """
+    print("Step", self.number_of_steps, "- Open List: [")
+    [print("\t", node.game_state.get_data_string()) for node in self.open_list]
+    print("]")
+    print("Step", self.number_of_steps, "- Closed List: [")
+    [print("\t", node.game_state.get_data_string()) for node in self.closed_list]
+    print("]")
 
-  @abstractmethod
-  def search_for_solution(self): # TO BE OVERRIDDEN IN CHILD CLASS (Might not have to be) !!!
+  def search_for_solution(self):
     """
     Method to find the solution of the initial Node/GameState
     """
-    pass
-  
+    self.print_current_state() # for testing
+    while (not self.is_open_list_empty()):
+      popped_node = self.pop_first_node_from_open_list()
+      if (self.is_node_goal_state(popped_node)):
+        return self.get_solution_path(popped_node)
+      else:
+        self.add_node_to_closed_list(popped_node)
+        self.add_successors_to_open_list(popped_node)
+      self.number_of_steps += 1
+      self.print_current_state() # for testing
+    return [] # No solution
+
   @abstractmethod
   def get_node_cost(self): # TO BE OVERRIDDEN IN CHILD CLASS
     """
@@ -34,15 +51,34 @@ class GenericSearchAlgorithm(ABC):
     """
     pass
 
-  @abstractmethod
-  def add_successors_to_open_list(self, node): # TO BE OVERRIDDEN IN CHILD CLASS
+  # MIGHT HAVE TO BE OVERRIDDEN IN CHILD CLASS (depending on algorithm)
+  def add_successors_to_open_list_extra_steps(self, node):
     """
-    Method to add successor Nodes to the open list (and remove from closed list - depending on algorithm used)
+    Helper method to apply extra steps to the add_successors_to_open_list method, depending on the algorithm used
     """
+    ### override this in astar with the following
+    #elif ((same_node_from_closed_list != None) and (successor_node.get_node_cost() < same_node_from_closed_list.get_node_cost())):
+    #  self.remove_node_from_closed_list(same_node_from_closed_list)
+    #  self.add_node_to_open_list_sorted(successor_node)
+    #elif ((same_node_from_open_list != None) and (successor_node.get_node_cost() < same_node_from_open_list.get_node_cost())):
+    #  self.remove_node_from_open_list(same_node_from_open_list)
+    #  self.add_node_to_open_list_sorted(successor_node)
     pass
+
+  def add_successors_to_open_list(self, node):
+    """
+    Method to add successor Nodes to the open list (and remove from same from closed/open list - depending on algorithm used)
+    """
+    for successor_node in node.generate_successor_nodes():
+      same_node_from_closed_list = self.get_new_node_from_closed_list(successor_node)
+      same_node_from_open_list = self.get_new_node_from_open_list(successor_node)
+      if (same_node_from_closed_list == None and same_node_from_open_list == None):
+        self.add_node_to_open_list_sorted(successor_node)
+      else:
+        self.add_successors_to_open_list_extra_steps(successor_node)
   
   # --------------
-  # Helper Methods (Remove if methods are not used in search_for_solution method) !!!
+  # Helper Methods
   # --------------
 
   # A
@@ -50,7 +86,7 @@ class GenericSearchAlgorithm(ABC):
     """
     Method to check if the open list is empty
     """
-    return bool(self.open_list)
+    return not bool(self.open_list)
 
   # B
   def pop_first_node_from_open_list(self):
@@ -133,3 +169,6 @@ class GenericSearchAlgorithm(ABC):
     Method to sort the open list
     """
     self.open_list.sort(key = lambda node: self.get_node_cost(node))
+
+#algo = GenericSearchAlgorithm("RRB...C.B...C.AA........EEGGG....... C3 B4 H1 F2", True, False, False)
+#algo.print_current_state()
