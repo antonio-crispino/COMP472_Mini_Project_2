5a8fd36047095454a8ae9aae32ee1488c5179645
antonio-crispino <77855308+antonio-crispino@users.noreply.github.com>
2022-11-30 22:48:16 -0500

    Modify tests

--- implementation/game_state_test.py
+++
@@ -1,7 +0,0 @@
-from game_state import GameState
-
-initial_gamestate = GameState.generate_initial_gamestate("RRB...C.B...C.AA........EEGGG....... C3 B4 H1 F2")
-successor_gamestates = initial_gamestate.generate_successor_gamestates()
-
-print(initial_gamestate.get_data_string(), "\tis_solution_state:", initial_gamestate.get_change()["is_solution_state"])
-[print(successor.get_data_string(), "\tis_solution_state:", successor.get_change()["is_solution_state"]) for successor in successor_gamestates]
--- implementation/node_test.py
+++
@@ -1,9 +0,0 @@
-from game_state import GameState
-from node import Node
-
-initial_gamestate = GameState.generate_initial_gamestate("RRB...C.B...C.AA........EEGGG....... C3 B4 H1 F2")
-initial_node = Node.generate_initial_node(initial_gamestate, True, False, False, None)
-successor_nodes = initial_node.generate_successor_nodes()
-
-print(initial_node.game_state.get_data_string(), "\tis_solution_state:", initial_node.game_state.get_change()["is_solution_state"])
-[print(successor.game_state.get_data_string(), "\tis_solution_state:", successor.game_state.get_change()["is_solution_state"]) for successor in successor_nodes]
