ccb3cd454b973fa003411a240e8bd64a563f0026
antonio-crispino <77855308+antonio-crispino@users.noreply.github.com>
2022-11-30 22:43:08 -0500

    Update Node class

--- implementation/node.py
+++ implementation/node.py
@@ -3,13 +3,13 @@ class Node:
   The Node class
   '''
   
-  def __init__(self, parent_node, game_state, depth, use_g, use_h, use_f, heuristic):
+  def __init__(self, parent_node, game_state, depth, use_g, use_h, use_f, heuristic): # TESTED
     """
     The Node Constructor
     """
     self.parent_node = parent_node                                # Node: The parent node of the current node (None if initial node)
     self.game_state = game_state                                  # GameState: The GameState object of the current node
-    self.action = game_state.change["direction_moved"]            # String: The action (direction) performed to arrive at the current node
+    self.action = game_state.get_change()["direction_moved"]      # String: The action (direction) performed to arrive at the current node
     self.depth = depth                                            # Number: The depth of the current node with respect to a tree representation
     self.heuristic = heuristic                                    # Function or None: The heuristic function (if no heuristic, then equals None)
     self.use_g = use_g                                            # Boolean: Whether or not to use the g(n) value
@@ -20,13 +20,13 @@ class Node:
     self.value_f = (self.value_g + self.value_h) if use_f else 0  # Number: f(n) - The estimate of the total cost along the path through n.
 
   @classmethod
-  def generate_initial_node(cls, game_state, use_g, use_h, use_f, heuristic = None):
+  def generate_initial_node(cls, game_state, use_g, use_h, use_f, heuristic = None): # TESTED
     """
     Class method that creates the first Node
     """
     return Node(None, game_state, 0, use_g, use_h, use_f, heuristic)
 
-  def generate_successor_nodes(self):
+  def generate_successor_nodes(self): # TESTED
     """
     Method that creates the child Nodes of the Node
     """
