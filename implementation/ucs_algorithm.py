b7044dbc0810704ffb52346f8d04106ba7736f1a
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-12-03 17:25:44 -0500

    fix parameters

--- implementation/ucs_algorithm.py
+++ implementation/ucs_algorithm.py
@@ -5,11 +5,11 @@ class UCSAlgorithm(GenericSearchAlgorithm):
   The UCSAlgorithm class (Uniform Cost Search)
   '''
 
-  def __init__(self, puzzle_string, heuristic): # TESTED
+  def __init__(self, puzzle_string): # TESTED
     """
     The UCSAlgorithm Constructor
     """
-    super().__init__(puzzle_string, True, False, False, heuristic)
+    super().__init__(puzzle_string, True, False, False, heuristic=None)
 
   # Implemented abstract method from parent class
   def get_node_cost(self, node): # TESTED
