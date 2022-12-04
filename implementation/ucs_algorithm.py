f2a3ae2b8b7164060a9217bedc84c03f5897a092
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-12-03 21:41:30 -0500

    small fix, for override and return of node

--- implementation/ucs_algorithm.py
+++ implementation/ucs_algorithm.py
@@ -16,17 +16,4 @@ class UCSAlgorithm(GenericSearchAlgorithm):
     """
     Method to get the cost of a Node
     """
-    return node.value_f
-  
-  # Overridden method from parent class
-  def add_successors_to_open_list_extra_steps(self, successor_node, same_node_from_closed_list, same_node_from_open_list): # TESTED
-    """
-    Helper method to apply extra steps to the add_successors_to_open_list method, depending on the algorithm used
-    Note: UCS implements Breadth First Search and re-sorts the nodes each time based on fuel cost g(n)
-    """
-    if ((same_node_from_closed_list is not None) and (self.get_node_cost(successor_node) < self.get_node_cost(same_node_from_closed_list))):
-      self.remove_node_from_closed_list(same_node_from_closed_list)
-      self.add_node_to_open_list_sorted(successor_node)
-    elif ((same_node_from_open_list is not None) and (self.get_node_cost(successor_node) < self.get_node_cost(same_node_from_open_list))):
-      self.remove_node_from_open_list(same_node_from_open_list)
-      self.add_node_to_open_list_sorted(successor_node)
+    return node.value_g
